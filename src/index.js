1d516b7bd65448b55c7b9d6e833c9796b351c21a
ryu1715 <72054735+ryu1715@users.noreply.github.com>
2022-04-28 20:44:23 +0900

    test commit test

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ document.getElementById("app").innerHTML = `
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-<p>hello</p>
+<p>
+  修正しました
+</p>
 `;
